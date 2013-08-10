7f65bbd46d76eeff0f5960b198156522f1c95a31
Alonzo Turner <alonzo.turner@subtextproductions.com>
2013-08-10 11:17:38 -0500

    FIXED: PHP throws notice for undefined index

--- install.php
+++ install.php
@@ -70,7 +70,7 @@ class com_subtextInstallerScript
 	 * UNINSTALL
 	 */
 	function uninstall( $parent ) {
-		if(!$this->release) $this->release = '1.0.1';
+		if(!isset($this->release)) $this->release = '1.0.1';
 		echo '<p>' . JText::sprintf('COM_SUBTEXT_MSG_SUCCESS_UNINSTALL', $this->release) . '</p>';
 	}
  
